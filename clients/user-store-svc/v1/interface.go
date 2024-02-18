d243892f1c2d6b71c78071dc045bac80e3b3106e
Vincent Vielle <viellevincentd+github@gmail.com>
2024-02-18 14:28:25 +0100

    Work on mocks

--- clients/user-store-svc/v1/interface.go
+++ clients/user-store-svc/v1/interface.go
@@ -6,6 +6,7 @@ import (
 	user_store_svc_v1_entities "github.com/Golerplate/contracts/clients/user-store-svc/v1/entities"
 )
 
+//go:generate mockgen -source interface.go -destination mocks/mock_user_store_svc.go -package user_store_svc_v1_mocks
 type UserStoreSvc interface {
 	CreateUser(ctx context.Context, req *user_store_svc_v1_entities.UserCreate) (*user_store_svc_v1_entities.User, error)
 	GetUserByID(ctx context.Context, id string) (*user_store_svc_v1_entities.User, error)
