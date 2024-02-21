373a8c84aefa839ce9c3cec9db90280003d7b613
Vincent Vielle <viellevincentd+github@gmail.com>
2024-02-21 19:27:21 +0100

    Modify user models

--- models/http/common/account/v1/account.go
+++ models/http/common/account/v1/account.go
@@ -1,16 +1,11 @@
 package models_http_common_account_v1
 
-import "time"
-
 type Account struct {
 	User *User `json:"user"`
 }
 
 type User struct {
-	ID               string    `json:"id"`
-	Username         string    `json:"username"`
-	IsAdmin          bool      `json:"is_admin"`
-	IsBanned         bool      `json:"is_banned"`
-	HasVerifiedEmail bool      `json:"has_verified_email"`
-	CreatedAt        time.Time `json:"created_at"`
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	IsAdmin  bool   `json:"is_admin"`
 }
--- models/http/common/account/v1/private_account.go
+++ models/http/common/account/v1/private_account.go
@@ -1,18 +1,12 @@
 package models_http_common_account_v1
 
-import "time"
-
 type PrivateAccount struct {
 	User *PrivateUser `json:"user"`
 }
 
 type PrivateUser struct {
-	ID               string    `json:"id"`
-	Username         string    `json:"username"`
-	Email            string    `json:"email"`
-	IsAdmin          bool      `json:"is_admin"`
-	IsBanned         bool      `json:"is_banned"`
-	HasVerifiedEmail bool      `json:"has_verified_email"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	IsAdmin  bool   `json:"is_admin"`
 }
