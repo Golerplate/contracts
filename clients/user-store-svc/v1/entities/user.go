a270cba2ddc8cbdc4b508f5657520af90a07698d
Bastien Rigaud <contact.teyz+github@gmail.com>
2024-03-04 17:07:45 +0100

    fix: delete ExternalID

--- clients/user-store-svc/v1/entities/user.go
+++ clients/user-store-svc/v1/entities/user.go
@@ -3,12 +3,11 @@ package user_store_svc_v1_entities
 import "time"
 
 type User struct {
-	ID         string    `json:"id"`
-	ExternalID string    `json:"external_id"`
-	Username   string    `json:"username"`
-	Email      string    `json:"email"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
+	ID        string    `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type UserCreate struct {
